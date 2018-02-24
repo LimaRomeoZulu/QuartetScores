cd2ce5de6f045ef9212ffaa6c06f88033ce7a245
linda <lindar@gluten.ae.local>
2018-02-24 11:58:22 +0100

    Experiments added

--- experiments/expList.py
+++ experiments/expList.py
@@ -2,5 +2,6 @@ exp_list = [
 	#1,	#Executes 1st experiment: Reading small dataset and run it with different thread settings
 	#2,	#Exceutes 2nd experiment: Comparing the internal and external version with 300 taxa and 64GB RAM available
 	#3,	#Executes 3rd experiment: Comparing the internal and external version with 300 taxa and 10 GB RAM available
-	4	#Executes 4th experiment: Comparing the runtime of a generated and a real tree with 200 taxa and 250 trees
+	4,	#Executes 4th experiment: Comparing the runtime of a generated and a real tree with 200 taxa and 250 trees
+	5	#Executes 5th experiment: Comparing the runtime of 2 different parallized versions
 ]
--- experiments/pipeline.py
+++ experiments/pipeline.py
@@ -2,6 +2,7 @@ import exp1
 import exp2
 import exp3
 import exp4
+import exp5
 import expList
 
 ################ Executing experiments  #########################
@@ -9,7 +10,8 @@ mapping = {
 	1 : exp1.experiment1,
 	2 : exp2.experiment2,
 	3 : exp3.experiment3,
-	4 : exp4.experiment4 
+	4 : exp4.experiment4, 
+	5 : exp5.experiment5
 }
 
 
